edf274f4f526263c88a46f9343cf25c5d8ecbfe8
Diego cervantes <diegocerbta@gmail.com>
2025-10-02 01:29:45 +0000

    Hola

---
+++ src/js/components/Bottom.jsx
@@ -0,0 +1,11 @@
+const Bottom = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark text-white">
+            <div className="container text-center">
+                <span>Copyright © Your Website 2025</span>
+            </div>
+        </footer>
+
+    )
+}
+export default Bottom
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -4,27 +4,29 @@ import React from "react";
 import NavBar from "./Navbar";
 import JumboTron from "./Jumbotron";
 import Cards from "./Cards";
-import Bottom from "../Bottom";
+import Bottom from "./Bottom";
 
 
 //create your first component
 const Home = () => {
 	return (
 		<div>
-			<div className="container-xxl container-md">
-				<NavBar />
-				
-			</div>
-			<div className="container-fluid container-md">
-				<JumboTron/>
-			</div>
-			<div className="container-fluid container-md">
-				<Cards/>
-			</div>
 			<div>
-				<Bottom/>
+				<div className="text-center">
+				<NavBar />
+				</div>
+				<JumboTron />
+				<Cards />
 			</div>
+
+			<div>
+				<footer>
+					<Bottom />
+				</footer>
+			</div>
+
 		</div>
+
 	);
 };
 
